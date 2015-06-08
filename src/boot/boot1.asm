9e1bd2438a924a66398928513538236bcbad69f0
PaulGap <paulgschwendtner@me.com>
2015-06-08 19:03:39 +0200

    IDT Keybinding :OO

--- src/boot/boot1.asm
+++ src/boot/boot1.asm
@@ -1,9 +1,25 @@
 [BITS 32]
 [EXTERN initKernel]
+[EXTERN callKeyboard]
+[EXTERN idtp]
 
 global _start
+global irq1
+global idt_load
+
 _start:
 	call initKernel
 
 	jmp $
 	RET
+
+idt_load:
+	lidt [idtp]
+	RET
+
+; ##### IRQS ####
+irq1:
+	call callKeyboard
+	mov al, 0x20
+	out 0x20, al
+	iret
