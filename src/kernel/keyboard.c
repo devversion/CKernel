452e479308727778d56b5d790f32a9cbb76a661c
PaulGap <paulgschwendtner@me.com>
2015-06-13 10:53:48 +0200

    Oh Yeah, Qwerty Upper and Lowercase Support, A lot of work...

--- src/kernel/keyboard.c
+++ src/kernel/keyboard.c
@@ -1,109 +1,34 @@
-unsigned char qwertylow[128] = {
-	0, // NULL
-	27, // ESC
-	'1',
-	'2',
-	'3',
-	'4',
-	'5',
-	'6',
-	'7',
-	'8',
-	'9',
-	'0',
-	'-',
-	'=',
-	'\b', // BACKSPACE
-	'\t', // TAB
-	'q',
-	'w',
-	'e',
-	'r',
-	't',
-	'y',
-	'u',
-	'i',
-	'o',
-	'p',
-	'[',
-	']',
-	'\n', // ENTER
-	0, // LEFT CONTROL
-	'a',
-	's',
-	'd',
-	'f',
-	'g',
-	'h',
-	'j',
-	'k',
-	'l',
-	';',
-	'\'',
-	'`',
-	0, // LEFT SHIFT
-	'\\',
-	'z',
-	'x',
-	'c',
-	'v',
-	'b',
-	'n',
-	'm',
-	',',
-	'.',
-	'/',
-	0, // RIGHT SHIFT
-	'*',
-	0, // LEFT ALT
-	' ',
-	0, // CAPS LOCK
-	0, // F1
-	0, // F2
-	0, // F3
-	0, // F4
-	0, // F5
-	0, // F6
-	0, // F7
-	0, // F8
-	0, // F9
-	0, // F10
-	0, // NUM LOCK
-	0, // SCROLL LOCK
-	0, // KEYPAD 7
-	0, // KEYPAD 8
-	0, // KEYPAD 9
-	0, // KEYPAD -
-	0, // KEYPAD 4
-	0, // KEYPAD 5
-	0, // KEYPAD 6
-	0, // KEYPAD +
-	0, // KEYPAD 1
-	0, // KEYPAD 2
-	0, // KEYPAD 3
-	0, // KEYPAD 0
-	0, // KEYPAD .
-	0, // EMPTY
-	0, // EMPTY
-	0, // EMPTY
-	0, // F11
-	0, // F12
-};
-
+#include "keyboard.h"
 
 unsigned char getLowerChar(unsigned char scancode) {
 	return qwertylow[scancode];
 }
 
 unsigned char getUpperChar(unsigned char scancode) {
-	//TODO CREATE UPPER CHAR KEYMAP
-	return qwertylow[scancode];
+	return qwertyup[scancode];
 }
 
 void callKeyboard() {
 	unsigned char scancode = in(0x60);
 
+	// HANDLE UPPERCASE
+	if ((scancode & 0x80)) {
+		if (scancode == 0xAA || scancode == 0xB6) {
+			uppercase = 0;
+		}
+	} else {
+		while (scancode == 0x36 || scancode == 0x2A) {
+			uppercase = 1;
+			scancode = in(0x60);
+		}
+	}
+
+	// PRINT CHAR
 	if (!(scancode & 0x80)) {
-		printChar(getLowerChar(scancode));
+		if (uppercase) {
+			printChar(getUpperChar(scancode));
+		} else {
+			printChar(getLowerChar(scancode));
+		}
 	}
 }
--- src/kernel/keyboard.h
+++ src/kernel/keyboard.h
@@ -1,6 +1,11 @@
 #pragma once
 #include "ports.h"
 
+unsigned char qwertylow[128];
+unsigned char qwertyup[128];
+
 unsigned char getLowerChar(unsigned char scancode);
 unsigned char getUpperChar(unsigned char scancode);
 void callKeyboard();
+
+unsigned int uppercase;
