cc891ae2eb9080ab87b4e5aeb99642b87862046e
PaulGap <paulgschwendtner@me.com>
2015-06-14 10:56:20 +0200

    Created Sleep Method.

--- src/kernel/util.c
+++ src/kernel/util.c
@@ -11,6 +11,30 @@ int convertBCD(int bcd) {
 	return (((bcd & 0xF0) >> 4) * 10) + (bcd & 0x0F);
 }
 
+void sleep(unsigned int milliseconds) {
+	int inseconds = milliseconds / 1000;
+	int inminutes = inseconds / 60;
+	int inhours = inminutes / 60;
+	inminutes = inminutes % 60;
+	inseconds = inseconds % 60;
+
+	inseconds += getSeconds();
+	inminutes += getMinutes();
+	inhours += getHours();
+
+	while (inseconds > 60) {
+		inminutes += 1;
+		inseconds -= 60;
+	}
+
+	while (inminutes > 60) {
+		inhours += 1;
+		inminutes -= 60;
+	}
+
+	while (getSeconds() <= inseconds && getMinutes() <= inminutes && getHours() <= inhours);
+}
+
 char *itoa(int num, char *str, int radix) {
 	char sign = 0;
 	char temp[17];
--- src/kernel/util.h
+++ src/kernel/util.h
@@ -1,3 +1,4 @@
 void memset(void *addr, unsigned char value, unsigned int len);
 int convertBCD(int bcd);
+void sleep(unsigned int milliseconds);
 char *itoa(int num, char *str, int radix);
